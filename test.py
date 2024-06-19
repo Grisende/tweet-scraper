1e852d44fe12663627eb9fe8b03d6b4dc870dd35
alexandre h grisende <alexandrehgrisende@hotmail.com>
2024-06-19 18:15:04 -0300

    removing useless code

--- test.py
+++ test.py
@@ -3,25 +3,6 @@ from dotenv import load_dotenv
 from scraper.login import Login
 from scraper.scraping import Scraping
 
-
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
-
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--no-sandbox")
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--disable-gpu")
-# chrome_options.add_argument('--disable-dev-shm-usage') 
-# driver = webdriver.Chrome(options=chrome_options)
-# driver.get("https://x.com/")
-# driver.maximize_window()
-
-
-#css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-poiln3
-#css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-poiln3
-#css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-poiln3
-
 load_dotenv()
 
 login = Login(
